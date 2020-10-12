21389ff775759ed14f677a3fa323e67874c28a3b
otaviodantas <otavio-dantas@live.com>
2020-10-12 11:02:26 -0300

    Adjust

--- README.md
+++
@@ -1,2 +0,0 @@
-# NPL-generator-dataset
-This repo creates a CSV that can be used as a dataset for NLP experiments.
--- teste.py
+++
@@ -1,7 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-# os.path.expanduser()
-load_dotenv('config.env')
-
-print(os.environ.get("WORDKEY"))
